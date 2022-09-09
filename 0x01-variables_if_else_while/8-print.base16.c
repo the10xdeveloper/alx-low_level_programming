5c38aaf5c9984d5c3d321c09b2ada2bafa5467dd
Chukwunonso Nwankpa <hello@chukwunonsonwankpa.me>
2022-09-09 13:28:08 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print.base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - this programs prints all bas64 numbers in lowercase
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char x;
+	int y;
+
+	for (y = 0; y < 10; y++)
+	{
+		putchar(y + '0');
+	}
+	for (x = 'a'; x < 'g'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
