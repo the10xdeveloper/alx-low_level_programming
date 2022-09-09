b28829dde243ba86c62d4f6d1e30460282a3b7a5
Chukwunonso Nwankpa <hello@chukwunonsonwankpa.me>
2022-09-09 10:00:37 -0700

    Positive anything is better than negative thinking

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive\n", n);
 	
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 
-	else
-		printf("%d is zero\n", n),
+	else if (n < 0 )
+		printf("%d is negative\n", n),
 	
 	return (0);
 }
