2bd4f57dfc101f06cc999f8905364423dad96f43
Chukwunonso Nwankpa <hello@chukwunonsonwankpa.me>
2022-09-13 16:45:34 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include main.h
+/**
+ * main - This program prints _putchar followed 
+ * by a new line.
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char u[] = "_putchar";
+	int x = 0;
+
+	while (u[x] != '\0')
+	{
+		_putchar(u[x]);
+		x++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
