25b03427dc31c38f0e768f816f1915aa21dac4f1
Chukwunonso Nwankpa <hello@chukwunonsonwankpa.me>
2022-09-13 18:12:55 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 /**
  * _islower - Entry point
  * Description: Checks if a character is in lowercase
+ * @c: the integer value it receives
  * Return: 1 if true. 0 if false.
  */
 int _islower(int c)
