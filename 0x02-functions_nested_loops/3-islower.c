20e821cf551d434f506d50068e39e67cf4a44541
Chukwunonso Nwankpa <hello@chukwunonsonwankpa.me>
2022-09-13 18:08:15 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * Description: Checks if a character is in lowercase
+ * Return: 1 if true. 0 if false.
+ */
+int _islower(int c)
+{
+	int i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+		return (0);
+	}
+}
